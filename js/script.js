58cd59d795f0247b19c8aa84999d9aeb4abf0377
Rahul Patni <rahulpatni1123@gmail.com>
2015-08-01 20:47:42 +0530

    Class 1

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// debugger
+// console.log("Script is running");
+// // var year = 0;
+// var num1 = "4";
+// var num2 = "sdghjgf";
+// console.log(num1 + num2);
+// function abc() {}
+// var num3 = 6789392;
+// // Anonymous Fn
+// var jkl = function () {
+//     //var i = 0;
+//     return true;
+// }
+
+// var xyz = (function () {
+//     var year = 0;
+//     console.log(year);
+//     return function incr() {
+//         return ++year;
+//     }
+// })();
+// console.log("---------------" + xyz());
+// console.log("=================" + xyz());
+// console.log("************" + xyz());
+
+
+// // Self invoking fn
+// (function(){
+//
+// console.log("II");
+// })();
+//
+//
+// var num4 = 68;
+// var num5;
+// add();
+// var add = function () {
+//
+//     alert(7+8);
+// }
+// (function(){
+//     $('h1').click(function(){
+//         alert(863485723);
+//     });
+// })();
